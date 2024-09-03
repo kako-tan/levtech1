38883b4fb2ea25df3ccdca7960243e1bed2fab4b
kako-tan <kakomatsumoto662@gmail.com>
2024-09-03 18:43:40 +0900

    カリキュラム8-5

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,8 +3,10 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        
     </head>
     <body>
         <h1 class='title'>
@@ -16,8 +18,11 @@
                 <p class='body'>{{ $post->body }}</p>
             </div>
         </div>
+        <div class='edit'>
+            <a href="/posts/{{ $post->id }}/edit">edit</a>
+        </div>
         <div class='footer'>
-            <a href='/'>戻る</a>
+            <a href="/">戻る</a>
         </div>
     </body>
 </html>
